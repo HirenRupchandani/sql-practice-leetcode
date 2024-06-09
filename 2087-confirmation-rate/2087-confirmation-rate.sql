4d9393465ac93022e1e01f76143ee04eabdc6a9d
Hiren Rupchandani <38413234+HirenRupchandani@users.noreply.github.com>
2024-06-09 13:31:32 -0400

    Time: 626 ms (97.56%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -1,9 +1,7 @@
-/* Write your T-SQL query statement below */
-/* Write your T-SQL query statement below */
--- declare @total float;
--- select @total = count(*) from confirmations;
-
-select s.user_id, ROUND((AVG(CASE WHEN action='confirmed' THEN 1.0 ELSE 0 END)),2) AS confirmation_rate 
-from Signups as s 
-left join Confirmations as c ON s.user_id = c.user_id
-group by s.user_id;
+# Write your MySQL query statement below
+select s.user_id, 
+round( sum(case when c.action='confirmed' then 1.0 else 0 end) / count(*) ,2) as confirmation_rate
+from signups s
+left join confirmations c
+on s.user_id = c.user_id
+group by s.user_id;
