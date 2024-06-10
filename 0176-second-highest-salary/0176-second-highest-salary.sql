e879f8492bac9dc4eb7ccd5ebfcdf701c9a12aec
Hiren Rupchandani <38413234+HirenRupchandani@users.noreply.github.com>
2024-06-10 02:44:33 -0400

    Time: 227 ms (98.7%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,11 +1,11 @@
 # Write your MySQL query statement below
-select ifnull(max(salary), null) as SecondHighestSalary 
-from employee
-where salary < (select max(salary) from employee);
+-- select ifnull(max(salary), null) as SecondHighestSalary 
+-- from employee
+-- where salary < (select max(salary) from employee);
 
 -- Another Solution from solutions:
--- select
--- (select distinct Salary 
--- from Employee order by salary desc 
--- limit 1 offset 1) 
--- as SecondHighestSalary;
+select
+(select distinct Salary 
+from Employee order by salary desc 
+limit 1 offset 1) 
+as SecondHighestSalary;
