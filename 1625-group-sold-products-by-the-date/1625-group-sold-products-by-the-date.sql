e1923366ebb9a789399560beba8d85b110e2e5c6
Hiren Rupchandani <38413234+HirenRupchandani@users.noreply.github.com>
2024-06-10 02:49:22 -0400

    Time: 363 ms (99.94%), Space: 0B (100%) - LeetHub

--- 1625-group-sold-products-by-the-date/1625-group-sold-products-by-the-date.sql
+++ 1625-group-sold-products-by-the-date/1625-group-sold-products-by-the-date.sql
@@ -2,3 +2,9 @@
 select a.sell_date, count(distinct a.product) as num_sold, group_concat(distinct a.product order by a.product asc SEPARATOR ',') as products
 from Activities a
 group by a.sell_date;
+
+-- PostgreSQL:
+-- SELECT column_to_group_by,
+--        STRING_AGG(words_column, ' ' ORDER BY words_column) AS concatenated_words
+-- FROM your_table
+-- GROUP BY column_to_group_by;
