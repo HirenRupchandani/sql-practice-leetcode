bc21cc678e884d24e771827181bd27ff9cb9802b
Hiren Rupchandani <38413234+HirenRupchandani@users.noreply.github.com>
2024-06-10 03:02:15 -0400

    Time: 706 ms (95.78%), Space: 0B (100%) - LeetHub

---
+++ 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select *
+from users
+where mail REGEXP '^[a-zA-Z][a-zA-Z0-9._-]*@leetcode\\.com$';
+-- where mail REGEXP '^[a-zA-Z][a-zA-Z0-9._-]*@leetcode\\.com';
