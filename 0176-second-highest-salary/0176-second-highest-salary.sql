c51ae1ad9b70ba969dc8202b7357b34b0d9efdf3
Hiren Rupchandani <38413234+HirenRupchandani@users.noreply.github.com>
2024-06-10 02:44:04 -0400

    Time: 1225 ms (5.02%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -2,3 +2,10 @@
 select ifnull(max(salary), null) as SecondHighestSalary 
 from employee
 where salary < (select max(salary) from employee);
+
+-- Another Solution from solutions:
+-- select
+-- (select distinct Salary 
+-- from Employee order by salary desc 
+-- limit 1 offset 1) 
+-- as SecondHighestSalary;
